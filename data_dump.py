ab1e5efbecb8026eb7166cc4f66cc397be98d941
Anukriti <anukriti13901@gmail.com>
2023-05-08 22:13:30 +0530

    Initial commit

---
+++ data_dump.py
@@ -0,0 +1,18 @@
+import pymongo
+import pandas as pd  
+import json 
+
+client = pymongo.MongoClient("mongodb+srv://anukriti13901:anukriti2001@cluster0.q0grjys.mongodb.net/?retryWrites=true&w=majority")
+DATA_FILE_PATH = (r"F:\myprojects\Medical-Insurance\insurance.csv")
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+if __name__=="__main__":
+    df= pd.read_csv(DATA_FILE_PATH)
+    print(f"rows and columns: {df.shape}")
+
+    df.reset_index(drop=True, inplace= True)
+    json_record= list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)  
